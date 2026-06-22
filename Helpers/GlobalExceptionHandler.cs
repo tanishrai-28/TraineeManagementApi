a55719bf703bcbf20ac377471c7d43bc40ac0ebc
tanishrai-28 <tanish.rai@zeuslearning.com>
2026-06-22 17:37:23 +0530

    Update global exception handler for more detailed and structured error response.

--- Helpers/GlobalExceptionHandler.cs
+++ Helpers/GlobalExceptionHandler.cs
@@ -1,15 +1,25 @@
 using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 public class GlobalExceptionHandler: IExceptionHandler
 {
     public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
     {
-        httpContext.Response.StatusCode = 500;
-        await httpContext.Response.WriteAsJsonAsync(new
+        ProblemDetails problemDetails = new ()
         {
-            message = exception.Message,
-            status = 500
-        });
+            Detail = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Instance = httpContext.Request.Path
+        };
+
+        problemDetails.Extensions["traceId"] = httpContext.TraceIdentifier;
+        problemDetails.Extensions["timestamp"] = DateTime.UtcNow;
+
+        httpContext.Response.StatusCode = 500;
+        await httpContext.Response.WriteAsJsonAsync(
+            problemDetails,
+            cancellationToken
+        );
 
         return true;
     }
